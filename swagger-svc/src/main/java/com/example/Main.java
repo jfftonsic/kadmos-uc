a32efca500eb2d9b0760a2b0ff7baae2b0c116a6
Joao Fernando Franco Tonsic <tonsic@gmail.com>
2022-03-18 16:41:17 -0300

    Removing old stuff

--- swagger-svc/src/main/java/com/example/Main.java
+++ swagger-svc/src/main/java/com/example/Main.java
@@ -5,16 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
-    //
-    //    @Bean
-    //    public CommonsRequestLoggingFilter commonsRequestLoggingFilter() {
-    //        CommonsRequestLoggingFilter filter
-    //                = new CommonsRequestLoggingFilter();
-    //        filter.setIncludeQueryString(true);
-    //        filter.setIncludePayload(true);
-    //        filter.setMaxPayloadLength(10000);
-    //        return filter;
-    //    }
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
